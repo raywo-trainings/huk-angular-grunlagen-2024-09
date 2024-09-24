62d4cf5ba2149ca2c8ecbc09aef0081ece4228b7
Ray Wojciechowski <ray@skillbird.de>
2024-09-24 15:29:14 +0200

    Add functions to service

--- src/app/recipes/services/recipes.service.ts
+++ src/app/recipes/services/recipes.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Recipe} from "../model/recipe.model";
 import {recipes} from "../data/recipe.dummy.data";
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +10,22 @@ export class RecipesService {
 
   public getAllRecipes(): Recipe[] {
     return recipes
+      .sort((lhs: Recipe, rhs: Recipe) => {
+        return lhs.name.localeCompare(rhs.name)
+      })
+  }
+
+
+  public getRecipeAt(index: number): Recipe {
+    if (index > this.getAllRecipes().length - 1 || index < 0) {
+      throw new Error(`Index out of bounds index: ${index}, length: ${this.getAllRecipes().length}.`)
+    }
+
+    return this.getAllRecipes()[index]
+  }
+
+
+  public addRecipe(recipe: Recipe) {
+    this.getAllRecipes().push(recipe)
   }
 }
